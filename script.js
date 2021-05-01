645c88f0e0431e1c4fdf25b2bf8161989ce420a0
dghodse <ghodsednyaneshwar@gmail.com>
2021-05-01 10:14:54 +0530

    updated validation and store data

--- script.js
+++ script.js
@@ -10,14 +10,58 @@ function validateForm(){
     //contraint api
     isValid = form.checkValidity();
     // error msg
-    msg.textContent = 'Please fill out all fields.';
-    msg.style.color = 'red';
-    msgContainer.style.borderColor = 'red';
+    if(!isValid){
+        msg.textContent = "Please fill out all fields.";
+        msg.style.color = "red";
+        msgContainer.style.borderColor = "red";
+        return;
+    }
+
+    if(password1El.value === password2El.value){
+        passwordsMatch = true;
+        password1El.style.borderColor = 'green';
+        password2El.style.borderColor = "green";
+    }else {
+        passwordsMatch = false;
+        msg.textContent = "Make sure passwords match.";
+        msg.style.color = 'red';
+        msgContainer.style.borderColor = 'red';
+        password1El.style.borderColor = "red";
+        password2El.style.borderColor = "red";
+        return;
+    }
+
+    //if valid form & pass
+    if(isValid && passwordsMatch){
+        msg.textContent = "Successfully Registered!";
+        msg.style.color = 'green';
+        msgContainer.style.borderColor = 'green';
+    }
+}
+
+
+function storeFormData(){
+    const user = {
+      name: form.name.value,
+      phone: form.phone.value,
+      email: form.email.value,
+      website: form.website.value,
+      password: form.password.value,
+    };
+
+    //do something with user data
+    console.log(user);
 }
 
 function processFormData(e){
     e.preventDefault();
+    //validate
     validateForm();
+    //submit if valid
+    if(isValid && passwordsMatch){
+        storeFormData();
+    }
+    
 }
 
 //Event listener
